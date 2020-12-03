96fb58d01bd173fc94be55729538be9e17105756
erindmehmeti <erindmehmeti@gmail.com>
2020-12-04 00:02:31 +0100

    initial commit

---
+++ src/notification.js
@@ -0,0 +1,14 @@
+export  function notify(message){
+    alert(message);
+};
+
+
+export  function log(message){
+    alert(message);
+};
+
+
+export default {
+    notify: notify,
+    log: log
+};
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require('webpack');
+
+var path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    }, 
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: 'css-loader'
+            }
+        ]
+    }
+};
